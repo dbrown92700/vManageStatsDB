c91a67f119049bb155239aae2b50cb61e5512f8b
David Brown <dbrown92700@gmail.com>
2022-10-26 21:18:48 -0400

    added target directory

--- includes.py
+++ includes.py
@@ -1,4 +1,5 @@
 # vManage server(s) and credentials
 vmanage_ip = 'vmanage.sdwan.cisco.com:8443'
-vmanage_user = 'userid'
-vmanage_password = 'mypassword'
+vmanage_user = 'davibrow'
+vmanage_password = 'password'
+file_directory = './results'
